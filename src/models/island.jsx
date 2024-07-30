4ae4cab324e6471effeeadfabe51e62c7e072add
PMurphyB <pmurphyguitar@gmail.com>
2024-07-30 11:13:18 -0600

    Adejusted Normalized Rotation

--- src/models/island.jsx
+++ src/models/island.jsx
@@ -80,17 +80,19 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props}) => {
 
       const normalizedRotation = ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
 
+      console.log(normalizedRotation)
+
       switch (true) {
-        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85 :
+        case normalizedRotation >= 4.1 && normalizedRotation <= 4.4 :
           setCurrentStage(4);
           break;
-        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3 :
+        case normalizedRotation >= 2.8 && normalizedRotation <= 3.1 :
           setCurrentStage(3);
           break;
-        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6 :
+        case normalizedRotation >= 1.4 && normalizedRotation <= 1.7 :
           setCurrentStage(2);
           break;
-        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75 :
+        case normalizedRotation >= 5.7 && normalizedRotation <= 6 :
           setCurrentStage(1);
           break;
         default :
