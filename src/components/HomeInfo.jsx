4229b44981588f28d6baa1a9620f4e089aee0329
PMurphyB <pmurphyguitar@gmail.com>
2024-07-31 11:06:48 -0600

    Added arrow icon and implemented it

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { arrow } from '../assets/icons'
+
+const InfoBox = ({ text, link, btnText }) => (
+  <div className="info_box">
+    <p className="font-medium sm:text-xl text-center">{text}</p>
+    <Link to={link} className="neo-brutalism-white neo-btn">
+      {btnText}
+      <img src={arrow} className="w-4 h-4 object-contain" />
+    </Link>
+  </div>
+)
 
 const renderContent = {
   1: (
     <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
       Hi I am <span className="font-semibold">Payton</span> 👋
+      <br/>
+      A Software Engineer from Olathe, KS
     </h1>
   ),
   2: (
-    <h1>2</h1>
+    <InfoBox 
+      text="One of my favorite things to do is play music!  I have competed in multiple competitions and have played across the country."
+      link="/about"
+      btnText="Learn more"
+    />
   ),
   3: (
     <h1>3</h1>
@@ -16,12 +34,6 @@ const renderContent = {
     <h1>4</h1>
   ),
 }
-
-const InfoBox = ({ text, link, btnText }) => (
-  <div className="info_box">
-    {text}
-  </div>
-)
 
 const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] || null;
