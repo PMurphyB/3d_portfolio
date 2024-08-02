7f065d3303f8be67d1737e4adfecc7ea729fbc1e
PMurphyB <pmurphyguitar@gmail.com>
2024-08-02 09:50:20 -0600

    Added reminder comments

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -13,6 +13,7 @@ const InfoBox = ({ text, link, btnText }) => (
   </div>
 )
 
+// Readjust currentStage render?
 const renderContent = {
   5: (
     <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
