b2473c52fbb8156d58fc76a4a9466c33f42048da
PMurphyB <pmurphyguitar@gmail.com>
2024-08-15 11:13:50 -0600

    Implemented Submit Button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' })
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+
+  };
   const handleFocus = () => {};
   const handleBlur = () => {};
+  const handleSubmit = () => {};
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in touch</h1>
 
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form 
+        className='w-full flex flex-col gap-7 mt-14'
+        onSubmit={handleSubmit}
+        >
           <label className='text-black-500 font-semibold'>
             Name
             <input type='text'
@@ -53,8 +61,14 @@ const Contact = () => {
             />
           </label>
 
-          <button>
-            
+          <button
+          type='submit'
+          className='btn'
+          disabled={isLoading}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
           </button>
 
         </form>
