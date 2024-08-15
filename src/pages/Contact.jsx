ef238cf72c66bb8020abf396dfb8b4d0f3e2075f
PMurphyB <pmurphyguitar@gmail.com>
2024-08-15 10:55:56 -0600

    Finished Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
   const [form, setForm] = useState({ name: '', email: '', message: '' })
 
   const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
@@ -13,7 +15,40 @@ const Contact = () => {
         <form className='w-full flex flex-col gap-7 mt-14'>
           <label className='text-black-500 font-semibold'>
             Name
-            <input type='text' name='name' className='input' placeholder='John' required value={form.name} onChange={handleChange}/>
+            <input type='text'
+            name='name'
+            className='input'
+            placeholder='John'
+            required value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlue={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+            name='message'
+            rows={4}
+            className='textarea'
+            placeholder='Let me know what I can do for you!'
+            required value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlue={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input type='text'
+            name='name'
+            className='input'
+            placeholder='John'
+            required value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlue={handleBlur}
+            />
           </label>
         </form>
       </div>
