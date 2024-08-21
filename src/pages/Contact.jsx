2ef381914bdca02106137d2804470efa347108f8
PMurphyB <pmurphyguitar@gmail.com>
2024-08-21 11:19:18 -0600

    Imported Fox for Display

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, Suspense } from 'react'
 import emailjs from '@emailjs/browser'
-
 import { Canvas } from '@react-three/fiber'
+
+import Fox from '../models/Fox';
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -107,8 +108,14 @@ const Contact = () => {
       </div>
 
       <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
-        <Canvas>
-
+        <Canvas
+          camera={{
+            position:[0, 0, 5]
+          }}
+        >
+          <Suspense fallback={null}>
+            <Fox />
+          </Suspense>
         </Canvas>
       </div>
     </section>
