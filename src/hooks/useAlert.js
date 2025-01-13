f06ffb8d9f068e41f9ff379cd5ba97456291c4bf
PMurphyB <pmurphyguitar@gmail.com>
2025-01-13 08:54:18 -0600

    Setup shwoAlert

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 const useAlert = () => {
   const [alert, setalert] = useState({ show: false, text: '', type: 'danger'})
 
+  const showAlert = () => setalert({ })
+
   return (
     <div>useAlert</div>
   )
