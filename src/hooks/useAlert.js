74147b5819cba96b267fabd347dc0a6f38c0ee2b
PMurphyB <pmurphyguitar@gmail.com>
2025-01-14 11:19:57 -0600

    Setting Up Alert

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const useAlert = () => {
   const [alert, setalert] = useState({ show: false, text: '', type: 'danger'})
 
-  const showAlert = () => setalert({ })
+  const showAlert = ({ text, type = 'danger' }) => setalert({ })
 
   return (
     <div>useAlert</div>
